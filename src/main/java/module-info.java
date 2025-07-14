0a22182aaba3de59673c21b4ed58ef05c46ed3dd
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-07-14 14:10:51 -0300

    fix: correção de configuração do módulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,14 @@
 module org.example.projeto_ia {
     requires javafx.controls;
     requires javafx.fxml;
+    
+    requires transitive javafx.graphics;
 
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
     opens org.example.projeto_ia to javafx.fxml;
     exports org.example.projeto_ia;
+    
+    exports org.example.qlearning;
 }
