1b9779ced269d5314cbf585a71b7992eee4315d6
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-07-21 12:20:50 -0300

    modificação do module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,11 @@
-module org.example.projeto_ia {
+module org.example.projeto.ia {
     requires javafx.controls;
     requires javafx.fxml;
-    
     requires transitive javafx.graphics;
-
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
     opens org.example.projeto_ia to javafx.fxml;
     exports org.example.projeto_ia;
-    
     exports org.example.qlearning;
 }
